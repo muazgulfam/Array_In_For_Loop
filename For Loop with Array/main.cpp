a6654daf596158dc82bd29577a28d3bb8dff8026
Muaz Bin Gulfam <106175817+muazgulfam@users.noreply.github.com>
2022-08-21 11:08:09 +0500

    Add files via upload

---
+++ For Loop with Array/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int scores [10] {12, 14, 16, 18, 20};
+    for (int i{0}; i < 8; i++) {
+        cout << scores [i] << endl;
+}
+
+
+
+    return 0;
+}
